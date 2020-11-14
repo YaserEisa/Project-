6a0e3fe6983235f4871d041124d5802dc128da16
Larry Yang <ly23@njit.edu>
2020-11-13 20:32:14 -0500

    switch from sqlit3 to postgresql for Heroku support https://devcenter.heroku.com/articles/sqlite3

--- db/migrate/20201026041228_add_saddress_to_users.rb
+++ db/migrate/20201026041228_add_saddress_to_users.rb
@@ -1,7 +1,8 @@
 class AddSaddressToUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :street_address
-    remove_column :users, :zip_code
+#    remove_column :users, :street_address
+#     PG::UndefinedColumn: ERROR:  column "street_address" of relation "users" does not exist
+#    remove_column :users, :zip_code
     add_column :users, :street_address, :string
     add_column :users, :city, :string
     add_column :users, :state, :string
